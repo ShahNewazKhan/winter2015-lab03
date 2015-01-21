b5ec23c98023020a4a0dc1f637508a75de10dd97
Shah <shahnewazk@gmail.com>
2015-01-21 10:39:53 -0800

    Completed index() in First.php to pull the first quote from the Quotes.php model and render to justone.php template
    Signed-off-by: Shah <shahnewazk@gmail.com>

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -9,6 +9,16 @@ class First extends Application
 
 	function index()
 	{
-		
+		$this->data['pagebody'] = 'justone';
+
+		$firstAuth = $this->quotes->get(1);
+
+		$this->data['what'] = $firstAuth['what'];
+
+		$this->data['who'] = $firstAuth['who'];
+
+		$this->data['mug'] = $firstAuth['mug'];
+
+		$this->render();
 	}
 }
